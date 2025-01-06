e8e973725d1824aef494d2078f6c34253d9547f0
Asam <abbasaliaboubakar@gmail.com>
2025-01-06 13:10:40 +0100

    remove animate

--- frontend/src/layouts/default.layout.tsx
+++ frontend/src/layouts/default.layout.tsx
@@ -1,35 +1,4 @@
-import { AnimatePresence } from "framer-motion";
-import Link from "next/link";
-import { motion } from "framer-motion";
-
 interface DefaultLayoutProps extends React.HTMLAttributes<HTMLDivElement> {}
-
-const navs: { name: string; path: string }[] = [
-  {
-    name: "Home",
-    path: "/",
-  },
-  {
-    name: "About",
-    path: "/about",
-  },
-  {
-    name: "Contact",
-    path: "/contact",
-  },
-];
-
-const slide = {
-  initial: {
-    top: "100vh",
-  },
-  enter: {
-    top: "100vh",
-  },
-  exit: {
-    top: "0",
-  },
-};
 
 export const DefaultLayout = ({
   children,
@@ -38,26 +7,9 @@ export const DefaultLayout = ({
 }: DefaultLayoutProps) => {
   return (
     <>
-      <AnimatePresence mode="wait">
-        <header className="bg-blue-800 py-4">
-          <div className="container mx-auto">
-            <div className="flex space-x-4 items-center">
-              {navs.map((i) => (
-                <Link
-                  key={i.path}
-                  className="text-white font-normal"
-                  href={i.path}
-                >
-                  {i.name}
-                </Link>
-              ))}
-            </div>
-          </div>
-        </header>
-        <main className={className} {...props}>
-          {children}
-        </main>
-      </AnimatePresence>
+      <main className={className} {...props}>
+        {children}
+      </main>
     </>
   );
 };
