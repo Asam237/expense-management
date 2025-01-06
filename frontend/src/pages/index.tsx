9b862a4f2997e7cd041bcc91aad94b76aaf171c4
Asam <abbasaliaboubakar@gmail.com>
2025-01-06 13:04:45 +0100

    redirect page

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,27 +1,17 @@
+import { useEffect } from "react";
+import { useRouter } from "next/router";
 import { DefaultLayout } from "@/layouts/default.layout";
-import { AnimatePresence } from "framer-motion";
-import Link from "next/link";
 
 const Home = () => {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.push("/signin");
+  }, [router]);
+
   return (
     <DefaultLayout>
-      <div className="container mx-auto p-4">
-        <h4>Home</h4>
-        <p>
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sit incidunt
-          minus nesciunt pariatur laudantium, dolore impedit dignissimos
-          dolorum, accusamus quidem veniam excepturi aperiam saepe, veritatis
-          quibusdam amet repudiandae ducimus harum maiores assumenda voluptates?
-          Corrupti expedita illo quis provident ex explicabo dolor fugit!
-          Assumenda, soluta numquam totam accusantium nemo illum veniam
-          voluptatibus perferendis earum aliquam incidunt voluptate fugit
-          dolorem pariatur nihil asperiores, beatae itaque quos sunt! Quod
-          officiis dicta deserunt culpa nemo sunt! Facilis tempora nemo deleniti
-          blanditiis impedit cupiditate iste magni quam corrupti numquam quia
-          iure alias quas facere, cumque veritatis consectetur quis doloribus
-          quos deserunt earum inventore voluptas est!
-        </p>
-      </div>
+      <div className="container mx-auto p-4"></div>
     </DefaultLayout>
   );
 };
